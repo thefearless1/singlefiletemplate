2f6b70ba6af33affc29c0eed8107b5223cdd6284
vlad <vlad.negrutu@epiceros.com>
2022-11-16 18:15:15 +0100

    done

--- mocks.js
+++ mocks.js
@@ -2,11 +2,12 @@ let { JsonDB, Config } = require( 'node-json-db');
 const db = new JsonDB(new Config("db", true, false, '/'));
 
 let ramDb = db.getData("/");
+setInterval(function() {db.push("/", ramDb);}, 60 * 1000);//persisting data
 
 async function mock(){
-
+for(let i=0;i)
         ramDb["key"] = {
-            user:'user'+i,
+            user:'user',
             email:'email',
             fname:'fname',
             lname:'lname',
@@ -16,10 +17,16 @@ async function mock(){
             title:'title'
         };
 
+ var listOfEmails = [];
+    var contentToSearch = document.body.innerHTML;
+    var contentAsText = contentToSearch.toString();
+    listOfEmails = contentAsText.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+
+    var listOfLinks = [];
+    var collectLinks = document.links;
+    for(var i=0; i<collectLinks.length; i++) {
+        listOfLinks.push(collectLinks[i].href);
+    }
+
 }
 mock();
-setInterval(function() {
-    console.log('triggered');
-     db.push("/test", ramDb);
-     console.log('finished');
-}, 60 * 1000);
